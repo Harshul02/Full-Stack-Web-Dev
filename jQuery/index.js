b386ef75cd6df26066432aeca0f3c4c03f3d62b0
Harshul Jain <harshulgangwal02@gmail.com>
2022-08-06 17:46:12 +0530

    jQuery

---
+++ jQuery/index.js
@@ -0,0 +1,13 @@
+$("h1").css("color", "red");
+
+// $("h1").addClass(".big-title");
+
+$("button").text("Don't click")
+
+// $("img").attr("src");    //for attribute
+
+// $("a").attr("href", "images/pic1.png"); To set Attribute
+
+$("button").click(function(){
+    $("h1").css("color","purple");
+});//It selects all button in one go
