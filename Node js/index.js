7ac6688dca0b220e9b9bf0f4a2aa5f9887b3b250
Harshul Jain <harshulgangwal02@gmail.com>
2022-08-08 00:25:36 +0530

    Node js

---
+++ Node js/index.js
@@ -0,0 +1,11 @@
+// const superheroes = require('superheroes');
+
+// var mysuperheroname = superheroes.random();
+
+// console.log(mysuperheroname);
+
+const supervillains = require('supervillains');
+
+var villain= supervillains.random();
+
+console.log(villain);
